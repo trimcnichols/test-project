e5d0f7ff7f5d1f44633d8c58088776954d36a077
tetap semangat <trimcnichols@outlook.com>
2023-03-06 16:06:21 -0500

    ddfile

---
+++ src/test/java/com/revature/driver.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+public class driver {
+    public static void main(String[] args) {
+
+        if (args.length > 0)
+            System.out.println("Hello World from" + args[0]);
+        else
+            System.out.println(" hello world pajama");
+    }
+}
